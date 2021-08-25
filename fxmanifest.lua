25a4ed3ce96565c34713cba4c75370a668673ef0
kibook <kibuzhj@gmail.com>
2021-08-25 16:31:47 -0400

    Add metadata to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,10 @@
 fx_version "cerulean"
 game "common"
 
+name "callbacks"
+description "Network callbacks library for FiveM and RedM"
+author "kibukj"
+repository "https://github.com/kibook/callbacks"
+
 server_script "server.lua"
 client_script "client.lua"
